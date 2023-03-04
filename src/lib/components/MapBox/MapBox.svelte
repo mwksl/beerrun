07c2f657d23d3e0d38d6760b37882c8bbe46e51f
mwksl <matthew@comet.fm>
2023-03-04 10:26:31 -0700

    feat: add bar crawl mode

--- src/lib/components/MapBox/MapBox.svelte
+++ src/lib/components/MapBox/MapBox.svelte
@@ -6,6 +6,7 @@
 	export let center: LngLatLike = [0, 0];
 	export let breweries;
 	export let clusteredBreweries;
+	export let barCrawl;
 
 	let mapElement: HTMLElement;
 	let map: mapboxgl.Map;
@@ -15,7 +16,7 @@
 
 	const fetchOptimization = async (coordinates) => {
 		const response = await fetch(
-			`https://api.mapbox.com/optimized-trips/v1/mapbox/walking/${coordinates}?access_token=${accessToken}`
+			`https://api.mapbox.com/optimized-trips/v1/mapbox/walking/${coordinates}?&overview=full&steps=true&geometries=geojson&source=first&access_token=${accessToken}`
 		);
 		return await response.json();
 	};
@@ -47,7 +48,7 @@
 		});
 
 		updateMarkers(breweries);
-		getRaceRoute(clusteredBreweries);
+		getRaceRoute(clusteredBreweries, barCrawl);
 	}
 
 	function updateMarkers(breweries) {
@@ -68,35 +69,66 @@
 		});
 	}
 
-	async function getRaceRoute(clusteredBreweries) {
+	async function getRaceRoute(clusteredBreweries, barCrawl) {
 		if (!center || !clusteredBreweries) return;
 
 		// Get directions between breweries
 		let breweryCoordinates = '';
-		for (const distance in clusteredBreweries) {
-			if (breweryCoordinates) {
-				breweryCoordinates += ';'; // add semicolon if breweryCoordinates already contains coordinates
-			}
-			breweryCoordinates += clusteredBreweries[distance]
+		let start = '';
+
+		if (barCrawl) {
+			// add all the 5km breweries to the breweryCoordinates string
+			breweryCoordinates += clusteredBreweries['5k']
 				.map((brewery) => `${brewery?.longitude},${brewery?.latitude}`)
 				.join(';');
+
+			// we want to end where we started, so add the first brewery to the end of the string
+			breweryCoordinates += `;${clusteredBreweries['5k'][0]?.longitude},${clusteredBreweries['5k'][0]?.latitude}`;
+		} else {
+			start = `${center[0]},${center[1]};`;
+			for (const distance in clusteredBreweries) {
+				if (breweryCoordinates) {
+					breweryCoordinates += ';'; // add semicolon if breweryCoordinates already contains coordinates
+				}
+				breweryCoordinates += clusteredBreweries[distance]
+					.map((brewery) => `${brewery?.longitude},${brewery?.latitude}`)
+					.join(';');
+			}
 		}
 		// strip off the last semicolon
 		breweryCoordinates = breweryCoordinates.slice(0, -1);
 		let directions;
 		try {
-			directions = await fetchDirections(`${center[0]},${center[1]};${breweryCoordinates}`);
+			if (barCrawl) {
+				directions = await fetchOptimization(breweryCoordinates);
+			} else {
+				directions = await fetchDirections(`${start}${breweryCoordinates}`);
+			}
 		} catch (error) {
 			console.log(error);
 			return;
 		}
 
+		let routeCoordinates;
 		// Extract the coordinates from the directions response
-		const routeCoordinates = directions.routes[0]?.geometry.coordinates;
-		if (!routeCoordinates) return; // return if routeCoordinates is undefined
+		if (!barCrawl) {
+			routeCoordinates = directions.routes[0]?.geometry.coordinates;
+		} else {
+			// if barCrawl, we need to get the coordinates from the waypoints
+			routeCoordinates = directions?.trips[0]?.geometry.coordinates;
+		}
 
-		// Check if the route layer already exists on the map and remove it if it does
-		if (map.getLayer('route')) {
+		if (!routeCoordinates) return;
+
+		// Route arrows must be removed first
+		const arrowLayer = map.getLayer('routearrows');
+		const routeLayer = map.getLayer('route');
+
+		if (typeof arrowLayer !== 'undefined') {
+			map.removeLayer('routearrows');
+		}
+
+		if (typeof routeLayer !== 'undefined') {
 			map.removeLayer('route');
 			map.removeSource('route');
 		}
@@ -124,13 +156,34 @@
 				'line-width': 5
 			}
 		} as mapboxgl.LineLayer);
+
+		map.addLayer(
+			{
+				id: 'routearrows',
+				type: 'symbol',
+				source: 'route',
+				layout: {
+					'symbol-placement': 'line',
+					'text-field': '▶',
+					'text-size': ['interpolate', ['linear'], ['zoom'], 12, 24, 22, 60],
+					'symbol-spacing': ['interpolate', ['linear'], ['zoom'], 12, 30, 22, 160],
+					'text-keep-upright': false
+				},
+				paint: {
+					'text-color': '#3887be',
+					'text-halo-color': 'hsl(55, 11%, 96%)',
+					'text-halo-width': 3
+				}
+			},
+			'waterway-label'
+		);
 	}
 	// This is called when the markers prop changes
 	$: updateMarkers(breweries);
 
 	$: center && map && map.setCenter(center);
 
-	$: if (clusteredBreweries) getRaceRoute(clusteredBreweries);
+	$: if (clusteredBreweries) getRaceRoute(clusteredBreweries, barCrawl);
 </script>
 
 <div class="h-1/2 w-full" bind:this={mapElement} />
---
+++ src/lib/components/Toggle.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let barCrawl
+	function toggleActive() {
+		barCrawl = !barCrawl;
+	}
+</script>
+
+<div class="form-control">
+	<label class="label cursor-pointer">
+		<span class="label-text">Bar Crawl?</span>
+		<input type="checkbox" class="toggle" on:change|preventDefault|stopPropagation={toggleActive} checked={barCrawl} />
+	</label>
+</div>
